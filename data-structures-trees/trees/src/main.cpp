961041538da5841a1a1371f943782fbf250641ee
Павел Павленко <neilnejl451@gmail.com>
2026-04-26 22:14:51 +0300

    Добавьте файлы проекта.

---
+++ data-structures-trees/trees/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include "../includes/binary_tree.h"
+#include "../includes/bst.h"
+#include "../includes/avl.h"
+#include "../includes/red_black.h"
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
