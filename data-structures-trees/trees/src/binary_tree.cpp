0c5cb082559131272a741167c0408cf925759ed9
Павел Павленко <neilnejl451@gmail.com>
2026-04-27 21:37:11 +0300

    Перевел проект на CMake

---
+++ data-structures-trees/trees/src/binary_tree.cpp
@@ -0,0 +1,7 @@
+#include "binary_tree.h"
+
+class BinaryTree
+{
+public:
+
+};
--- data-structures-trees/trees/src/main.cpp
+++ data-structures-trees/trees/src/main.cpp
@@ -1,14 +1,16 @@
 ﻿#include <iostream>
 
-#include "../includes/binary_tree.h"
-#include "../includes/bst.h"
-#include "../includes/avl.h"
-#include "../includes/red_black.h"
-
+#include "menu.h"
+#include "binary_tree.h"
+#include "bst.h"
+#include "avl.h"
+#include "red_black.h"
 
 int main()
 {
 
     std::cout << "Hello World!\n";
+
+
 }
 
