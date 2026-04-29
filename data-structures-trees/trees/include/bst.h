e5d7bed57e32452cfc3ebeb79b71bf38ddfdcb68
Павел Павленко <neilnejl451@gmail.com>
2026-04-29 16:25:18 +0300

    Создал файлики для БСТ

--- data-structures-trees/trees/include/bst.h
+++ data-structures-trees/trees/include/bst.h
@@ -1,1 +1,34 @@
 #pragma once
+
+class BST
+{
+private:
+
+	struct Node {
+		int value;
+		Node* left;
+		Node* right;
+
+
+		Node(int value);
+	};
+	Node* root;
+
+	void print(Node* node);
+	void deleteTree(Node* node);
+	void printPretty(Node* node, int depth, bool isLeft);
+
+public:
+	BST();
+	~BST();
+
+	void setRoot(int value);
+	Node* getRoot();
+
+	void addLeft(Node* parent, int value);
+	void addRight(Node* parent, int value);
+
+	void print();
+	void printPretty();
+
+};
